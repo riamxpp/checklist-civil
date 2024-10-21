c796ba14f7a6ed5ed5f46a721fbfddf78a84c6f7
Riam <riamstefeson616@gmail.com>
2024-10-21 17:41:21 -0300

    feat: criando view de POST para usuário, criando a rota para a nova view.

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserView
+from .views import UserDetaillView, UserCreateView
 
 urlpatterns = [
-  path('users/<int:pk>/', UserView.as_view(), name='get_user')
+  path('users/<int:pk>/', UserDetaillView.as_view(), name='user-detail'),
+  path('users/', UserCreateView.as_view(), name='user-create')
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from .models import User
 from .serializers import UserSerializer
 
-class UserView(APIView):
+class UserDetaillView(APIView):
   def get(self, request, pk): 
     try:
       user_data = User.objects.get(id=pk)
@@ -13,3 +13,16 @@ class UserView(APIView):
       return Response(serializer.data)
     except:
       return Response({ "Error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+class UserCreateView(APIView):
+  def post(self, request):
+    try:
+      serializer = UserSerializer(data=request.data)
+      print("aqui: ", serializer.is_valid())
+      if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+      return Response({ "Error": "Internal server error "}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    except:
+      return Response({ "Error": "Error creating user"}, status=status.HTTP_400_BAD_REQUEST)
