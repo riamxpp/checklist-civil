8fb0b971cb506397d7bc18ebe5c8f27855dcab8a
Riam <riamstefeson616@gmail.com>
2024-10-21 09:56:42 -0300

    feat: criando conexão com o banco de dados.

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  name = models.CharField(verbose_name="Nome", max_length=100, null=False, blank=False)
+  email = models.CharField(verbose_name="Email", max_length=100, null=False, blank=False)
