edaab297f97fb4ce3da148db487d0526d4ac7497
Riam <riamstefeson616@gmail.com>
2024-10-21 10:39:29 -0300

    feat: criando modelo de usuário no models.py

--- backend/user/models.py
+++ backend/user/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-  name = models.CharField(verbose_name="Nome", max_length=100, null=False, blank=False)
-  email = models.CharField(verbose_name="Email", max_length=100, null=False, blank=False)
+  name = models.CharField(verbose_name="Nome", max_length=100, blank=False)
+  email = models.CharField(verbose_name="Email", max_length=100, blank=False)
+  password = models.CharField(verbose_name="Senha", blank=False)
+  birth = models.DateField(verbose_name="Data de nascimento", null=True)
