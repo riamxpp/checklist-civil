a568882152e59f0a57069dd5bdcff1eee0edfe31
Riam <riamstefeson616@gmail.com>
2024-10-30 15:42:07 -0300

    feat: criando models de checklist. Configurando o settings.py para aceitar imagens.

--- backend/checklist/models.py
+++ backend/checklist/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Checklist(models.Model):
+  id_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="checklist")
+  title = models.CharField(max_length=255)
+  date_craetion = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return f"{self.title} - {self.id_user.name}"
+
+class Pergunta(models.Model):
+  EVALUATION_CHOICES = [
+    ('positivo', 'Positivo'),
+    ('ok', 'Ok'),
+    ('ruim', 'Ruim'),
+  ]
+
+  questionnaire = models.ForeignKey(Checklist, on_delete=models.CASCADE, related_name="perguntas")
+  text = models.TextField()
+  evaluation = models.CharField(max_length=50, choices=EVALUATION_CHOICES)
+  foto = models.ImageField(upload_to="perguntas_fotos/", blank=True, null=True)
+
+  def __str__(self):
+    return f"{self.text[:30]} - {self.evaluation}"
--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from decouple import config, Csv
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,6 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
